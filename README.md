346419e3fc54e4b65a2a40cd4fbd15b4eb8383aa
Markus Konrad <post@mkonrad.net>
2023-02-28 09:59:34 +0100

    get session ID via 'sess' URL parameter

--- README.md
+++ README.md
@@ -13,11 +13,15 @@ Es ist momentan online verfügbar unter https://rshiny.f4.htw-berlin.de/users/mk
 - zunächst muss das Paket [renv](https://rstudio.github.io/renv/index.html) installiert sein
 - dann können alle Paketabhängigkeiten installiert werden mittels `renv::restore()`
 
+## Entwicklung
+
+- alternativ zum "Run document" Knopf in RStudio kann `make devserver` verwendet werden, womit die Anwendung auf `http://localhost:8000` zur Verfügung gestellt wird
+
 ## Deployment auf HTW Server
 
 - das Deployment kann über die `Makefile` gesteuert werden
 - hierzu müssen ggf. erst die Pfade im `Makefile` angepasst werden, denn die beziehen sich auf den User `mk`
-- danach können mittels `make server_installdeps` die benötigten Pakete installiert werden
-- mittels `make server_testsync` kann simuliert werden, welche Dateien für das Deployment auf den Server kopiert werden würden
-- mittels `make server_sync` werden die Dateien dann tatsächlich auf den Server übertragen
-- mittels `make server_reload` wird die App auf dem Server neu gestartet – **das ist notwendig, damit die Änderungen nach einem Upload tatsächlich angezeigt werden**
+- danach können mittels `make installdeps` die benötigten Pakete installiert werden
+- mittels `make testsync` kann simuliert werden, welche Dateien für das Deployment auf den Server kopiert werden würden
+- mittels `make sync` werden die Dateien dann tatsächlich auf den Server übertragen
+- mittels `make reload` wird die App auf dem Server neu gestartet – **das ist notwendig, damit die Änderungen nach einem Upload tatsächlich angezeigt werden**
--- www/tutorial.js
+++ www/tutorial.js
@@ -1,4 +1,23 @@
+/**
+ * Extend JQuery with a function to get URL parameters.
+ */
+$.urlParam = function(name, url) {
+    if (!url) {
+     url = window.location.href;
+    }
+    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(url);
+    if (!results) {
+        return undefined;
+    }
+    return results[1] || undefined;
+}
+
+
+/**
+ * Set up the application using the configuration object `config`.
+ */
 function setup(config) {
+    // handling excluding elements by selector
     config.exclude.forEach(function(selector) {
         // hide element
         $(selector).hide();
@@ -12,6 +31,7 @@ function setup(config) {
         });
     });
     
+    // load additional JS files
     config.js.forEach(function(jsfile) {
         console.log("loading additional JS file", jsfile);
         $.getScript("./www/" + jsfile, function() {
@@ -19,14 +39,29 @@ function setup(config) {
         });
     });
     
+    // load additional CSS files
     config.css.forEach(function(cssfile) {
         console.log("loading additional CSS file", cssfile);
         $('head').append('<link rel="stylesheet" type="text/css" href="./www/' +  cssfile + '">');
     });
 }
 
+/**
+ * Global variables
+ */
 
+var sess = null;   // session ID
+
+/**
+ * Initialize the application when the HTML document was fully loaded.
+ */
 $(document).ready(function() {
+    // get session ID
+    if ($.urlParam('sess') !== undefined) {
+        sess = $.urlParam('sess');
+        console.log("using session ID", sess);
+    }
+    
     // load config from JSON
     fetch('./www/config.json')
         .then((response) => response.json())
